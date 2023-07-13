24a6f2b959ad939b182e4a6cafa744cc3b51342e
pdrygala <73122562+pdrygala@users.noreply.github.com>
2023-07-13 20:09:45 +0200

    Refactored code. Added export to file. Added goroutine that runs 1 time per hour

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
+// WeatherResponse represents the structure of the weather API response
 type WeatherResponse struct {
 	CurrentWeather struct {
 		Temperature   float64 `json:"temperature"`
@@ -20,6 +25,7 @@ type WeatherResponse struct {
 	} `json:"current_weather"`
 }
 
+// CityResponse represents the structure of the city API response
 type CityResponse struct {
 	Address struct {
 		HouseNumber  string `json:"house_number"`
@@ -31,39 +37,10 @@ type CityResponse struct {
 		Postcode     string `json:"postcode"`
 		Country      string `json:"country"`
 		CountryCode  string `json:"country_code"`
-	} `json: "address"`
+	} `json:"address"`
 }
 
-var weatherCodeMap = map[int]string{
-	0:  "Bezchmurne niebo",
-	1:  "Przeważnie bezchmurne",
-	2:  "Częściowo zachmurzone",
-	3:  "Zachmurzone",
-	45: "Mgła i szadź",
-	48: "Mgła i szadź",
-	51: "Mżawka: Słaba intensywność",
-	53: "Mżawka: Umiarkowana intensywność",
-	55: "Mżawka: Gęsta intensywność",
-	56: "Marznąca mżawka: Słaba intensywność",
-	57: "Marznąca mżawka: Gęsta intensywność",
-	61: "Deszcz: Słaba intensywność",
-	63: "Deszcz: Umiarkowana intensywność",
-	65: "Deszcz: Duża intensywność",
-	66: "Marznący deszcz: Słaba intensywność",
-	67: "Marznący deszcz: Duża intensywność",
-	71: "Opady śniegu: Słaba intensywność",
-	73: "Opady śniegu: Umiarkowana intensywność",
-	75: "Opady śniegu: Duża intensywność",
-	77: "Kryształki śniegu",
-	80: "Przelotne opady deszczu: Słaba intensywność",
-	81: "Przelotne opady deszczu: Umiarkowana intensywność",
-	82: "Przelotne opady deszczu: Wysoka intensywność",
-	85: "Przelotne opady śniegu: Słaba intensywność",
-	86: "Przelotne opady śniegu: Duża intensywność",
-	95: "Burza: Słaba intensywność",
-	96: "Burza z drobnym gradem",
-	99: "Burza z dużym gradem",
-}
+// WindDirection represents a wind direction range and description
 
 type WindDirection struct {
 	DegreeStart int
@@ -71,17 +48,50 @@ type WindDirection struct {
 	Description string
 }
 
-var windDirections = []WindDirection{
-	{DegreeStart: 335, DegreeEnd: 360, Description: "N"},
-	{DegreeStart: 295, DegreeEnd: 335, Description: "NW"},
-	{DegreeStart: 245, DegreeEnd: 295, Description: "W"},
-	{DegreeStart: 205, DegreeEnd: 245, Description: "SW"},
-	{DegreeStart: 155, DegreeEnd: 205, Description: "S"},
-	{DegreeStart: 115, DegreeEnd: 155, Description: "SE"},
-	{DegreeStart: 65, DegreeEnd: 115, Description: "E"},
-	{DegreeStart: 25, DegreeEnd: 65, Description: "NE"},
-	{DegreeStart: 0, DegreeEnd: 25, Description: "N"},
-}
+var (
+	weatherCodeMap = map[int]string{
+		0:  "Bezchmurne niebo",
+		1:  "Przeważnie bezchmurne",
+		2:  "Częściowo zachmurzone",
+		3:  "Zachmurzone",
+		45: "Mgła i szadź",
+		48: "Mgła i szadź",
+		51: "Mżawka: Słaba intensywność",
+		53: "Mżawka: Umiarkowana intensywność",
+		55: "Mżawka: Gęsta intensywność",
+		56: "Marznąca mżawka: Słaba intensywność",
+		57: "Marznąca mżawka: Gęsta intensywność",
+		61: "Deszcz: Słaba intensywność",
+		63: "Deszcz: Umiarkowana intensywność",
+		65: "Deszcz: Duża intensywność",
+		66: "Marznący deszcz: Słaba intensywność",
+		67: "Marznący deszcz: Duża intensywność",
+		71: "Opady śniegu: Słaba intensywność",
+		73: "Opady śniegu: Umiarkowana intensywność",
+		75: "Opady śniegu: Duża intensywność",
+		77: "Kryształki śniegu",
+		80: "Przelotne opady deszczu: Słaba intensywność",
+		81: "Przelotne opady deszczu: Umiarkowana intensywność",
+		82: "Przelotne opady deszczu: Wysoka intensywność",
+		85: "Przelotne opady śniegu: Słaba intensywność",
+		86: "Przelotne opady śniegu: Duża intensywność",
+		95: "Burza: Słaba intensywność",
+		96: "Burza z drobnym gradem",
+		99: "Burza z dużym gradem",
+	}
+
+	windDirections = []WindDirection{
+		{DegreeStart: 335, DegreeEnd: 360, Description: "N"},
+		{DegreeStart: 295, DegreeEnd: 335, Description: "NW"},
+		{DegreeStart: 245, DegreeEnd: 295, Description: "W"},
+		{DegreeStart: 205, DegreeEnd: 245, Description: "SW"},
+		{DegreeStart: 155, DegreeEnd: 205, Description: "S"},
+		{DegreeStart: 115, DegreeEnd: 155, Description: "SE"},
+		{DegreeStart: 65, DegreeEnd: 115, Description: "E"},
+		{DegreeStart: 25, DegreeEnd: 65, Description: "NE"},
+		{DegreeStart: 0, DegreeEnd: 25, Description: "N"},
+	}
+)
 
 func getWindDirection(degree int) string {
 	for _, direction := range windDirections {
@@ -92,35 +102,7 @@ func getWindDirection(degree int) string {
 	return "Unknown"
 }
 
-func parseDateTime(dateTimeStr string) (time.Time, error) {
-	// Parse the string into a time.Time value
-	layout := "2006-01-02T15:04"
-	dateTime, err := time.Parse(layout, dateTimeStr)
-	if err != nil {
-		return time.Time{}, err
-	}
-
-	return dateTime, nil
-}
-
-func convertToWarsawTime(dateTime time.Time) (time.Time, error) {
-	// Convert to Warsaw time (UTC+1)
-	location, err := time.LoadLocation("Europe/Warsaw")
-	if err != nil {
-		return time.Time{}, err
-	}
-	warsawTime := dateTime.In(location)
-
-	return warsawTime, nil
-}
-
-func formatDateTime(dateTime time.Time) string {
-	// Format the time in a easy-readable format
-	formattedTime := dateTime.Format("2006-01-02 15:04 MST")
-	return formattedTime
-}
-
-func getCity(latitude string, longitude string) []byte {
+func getCity(latitude string, longitude string) string {
 	// Get the city from the coordinates
 	url := fmt.Sprintf("https://geocode.maps.co/reverse?lat=%s&lon=%s", latitude, longitude)
 
@@ -136,32 +118,107 @@ func getCity(latitude string, longitude string) []byte {
 		fmt.Println("Error reading WeatherResponse body:", err)
 	}
 
-	return body
+	var jsonCityBody CityResponse
+
+	if err := json.Unmarshal(body, &jsonCityBody); err != nil {
+		fmt.Println("Error parsing JSON for city:", err)
+		return err.Error()
+	}
+
+	return jsonCityBody.Address.City
 }
 
-func getWeather(latitude string, longitude string) []byte {
-	// Get the weather from the coordinates
-	// https://api.open-meteo.com/v1/forecast?latitude=54.52&longitude=18.53&current_weather=true
+func writeToFile(fileName string, data []string) {
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	if err := writer.Write(data); err != nil {
+		panic(err)
+	}
+}
+
+func fetchWeather(latitude, longitude string) (WeatherResponse, error) {
 
 	host := "https://api.open-meteo.com/v1/forecast?"
 	params := fmt.Sprintf("latitude=%s&longitude=%s&current_weather=true", latitude, longitude)
-
 	url := host + params
 
 	resp, err := http.Get(url)
-
 	if err != nil {
 		fmt.Println("Error making Get request:", err)
 	}
-
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-
+	weatherBody, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Error reading WeatherResponse body:", err)
+		return WeatherResponse{}, err
 	}
-	return body
+	var jsonWeatherBody WeatherResponse
+
+	if err := json.Unmarshal(weatherBody, &jsonWeatherBody); err != nil {
+		fmt.Println("Error parsing JSON for weather:", err)
+		return WeatherResponse{}, err
+	}
+	return jsonWeatherBody, nil
+}
+
+func handleWeatherData(weatherData WeatherResponse, city string) {
+
+	formattedTime := formatDateTime(weatherData.CurrentWeather.Time)
+	temperature := fmt.Sprintf("%v", weatherData.CurrentWeather.Temperature)
+	windSpeed := fmt.Sprintf("%v", weatherData.CurrentWeather.WindSpeed)
+	weatherCode := weatherData.CurrentWeather.WeatherCode
+	direction := getWindDirection(int(weatherData.CurrentWeather.WindDirection))
+
+	fmt.Printf("Lokalizacja: %s \n", city)
+	fmt.Printf("Data pomiaru: %s \n", formattedTime)
+	description, exists := weatherCodeMap[weatherCode]
+	if exists {
+		fmt.Printf("Aktualna pogoda: %s\n", description)
+	} else {
+		fmt.Printf("Nieznany opis dla kodu pogody: %d\n", weatherCode)
+	}
+	fmt.Println("Temperatura:", temperature)
+	fmt.Println("Wiatr:", windSpeed)
+	fmt.Printf("Kierunek wiatru: %s\n", direction)
+
+	data := []string{
+		formattedTime,
+		city,
+		description,
+		temperature,
+		windSpeed,
+		direction,
+	}
+
+	writeToFile("data.csv", data)
+
+}
+
+func formatDateTime(dateTimeStr string) string {
+	dateTime, err := time.Parse("2006-01-02T15:04", dateTimeStr)
+	if err != nil {
+		fmt.Println("Error parsing date and time:", err)
+		return ""
+	}
+
+	// Convert to Warsaw time (UTC+1)
+	location, err := time.LoadLocation("Europe/Warsaw")
+	if err != nil {
+		fmt.Println("Error loading location:", err)
+		return ""
+	}
+	warsawTime := dateTime.In(location)
+
+	// Format the time in an easy-readable format
+	return warsawTime.Format("2006-01-02 15:04 MST")
 }
 
 func main() {
@@ -176,56 +233,25 @@ func main() {
 		fmt.Println("Example: <binary> --latitude=54.52 --longitude=18.53")
 	}
 
+	// Start a goroutine to fetch weather data every hour
+	go func() {
+		ticker := time.NewTicker(1 * time.Hour)
+		defer ticker.Stop()
 
+		for {
+			weatherData, err := fetchWeather(*latitude, *longitude)
+			if err != nil {
+				fmt.Println("Error fetching weather data:", err)
+			}
+			city := getCity(*latitude, *longitude)
 
-	weatherBody := getWeather(*latitude, *longitude)
-	cityBody := getCity(*latitude, *longitude)
-
-	var jsonWeatherBody WeatherResponse
-	var jsonCityBody CityResponse
-
-	err := json.Unmarshal(weatherBody, &jsonWeatherBody)
-	if err != nil {
-		fmt.Println("Error parsing JSON:", err)
-		return
-	}
-
-	err = json.Unmarshal(cityBody, &jsonCityBody)
-	if err != nil {
-		fmt.Println("Error parsing JSON:", err)
-		return
-	}
-
-	dateTime, err := parseDateTime(jsonWeatherBody.CurrentWeather.Time)
-	if err != nil {
-		fmt.Println("Error parsing date and time:", err)
-		return
-	}
-
-	warsawTime, err := convertToWarsawTime(dateTime)
-	if err != nil {
-		fmt.Println("Error converting time to Warsow", err)
-		return
-	}
-	formattedTime := formatDateTime(warsawTime)
-
-	city := jsonCityBody.Address.City
-
-	temperature := jsonWeatherBody.CurrentWeather.Temperature
-	windSpeed := jsonWeatherBody.CurrentWeather.WindSpeed
-	weatherCode := jsonWeatherBody.CurrentWeather.WeatherCode
-	direction := getWindDirection(int(jsonWeatherBody.CurrentWeather.WindDirection))
-
-	fmt.Printf("Lokalizacja: %s \n", city)
-	fmt.Printf("Data pomiaru: %s \n", formattedTime)
-	description, exists := weatherCodeMap[weatherCode]
-	if exists {
-		fmt.Printf("Aktualna pogoda: %s\n", description)
-	} else {
-		fmt.Printf("Nieznany opis dla kodu pogody: %d\n", weatherCode)
-	}
-	fmt.Println("Temperatura:", temperature)
-	fmt.Println("Wiatr:", windSpeed)
-	fmt.Printf("Kierunek wiatru: %s\n", direction)
-
+			handleWeatherData(weatherData, city)
+			<-ticker.C
+		}
+	}()
+	// Wait for a termination signal
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChannel
+	fmt.Println("Received termination signal. Exiting...")
 }
